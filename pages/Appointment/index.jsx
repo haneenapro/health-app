157facad0bc0f2dfe735f193b02871fef778e698
saran.baral <saranbrl35@gmail.com>
2023-07-17 21:53:15 +0545

    appointment page

--- pages/Appointment/index.jsx
+++ pages/Appointment/index.jsx
@@ -1,3 +1,4 @@
+"use client"
 // to fetch and display all information titles and little information
 
 import axios from "axios"
@@ -9,6 +10,7 @@ const HospitalList = () => {
   // api route -- get all informations
   const [isLoading, setIsLoading] = useState(false)
   const [informations, setInformations] = useState([])
+  const [searchValue, setSearchValue] = useState("")
 
   useEffect(() => {
     const getHospitals = async () => {
@@ -32,52 +34,63 @@ const HospitalList = () => {
     alert("One item deleted!!")
   }
 
+  const searchFilter = (_value) => {
+    setSearchValue(_value)
+  }
+
   return (
     <>
       <NavBar />
 
-      {/* Hospital List */}
-      <div class="mb-5 flex justify-between ">  
-        <h1 class="font-bold text-xl"> Choose Hospitals </h1>
-        
-        {/* <!-- Search Bar --> */}
-        
-        <form>   
+      <div className="container mx-auto">
+        {/* Hospital List */}
+        <div class="mb-5 flex justify-between mt-8">
+          <h1 class="font-bold text-xl"> Choose Hospitals </h1>
+
+          {/* <!-- Search Bar --> */}
+
+          <form>
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative ">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
-                </div>
-                <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Hospital Names ..." required />
-                <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+              <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Hospital Names ..."
+                onChange={(e) => searchFilter(e.target.value)} />
+              {/* <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
             </div>
-        </form>
+          </form>
         </div>
 
         {/* <!-- Card --> */}
-        {informations?.map((_hospital, _index)=>(
-            <div class="flex gap-x-3.5"> 
-                <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-700">
-                    <a href="#" class="items-center">
-                        <img class="rounded-t-lg self-center" src="./images/TH.jpg" alt="img" />
-                    </a>
-                    <div class="p-5">
-                        <a href="#">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black"> {_hospital.name} </h5>
-                        </a>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{_hospital.description}</p>
-                        <Link href={`/department?hospitalId=${_hospital.id}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Book an Appointment
-                            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                            </svg>
-                        </Link>
-                    </div>
-                </div>
+        {informations?.map((_hospital, _index) => (
+          <div class="flex gap-x-3.5">
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-700">
+              <a href="#" class="items-center">
+                <img class="rounded-t-lg self-center" src="./images/TH.jpg" alt="img" />
+              </a>
+              <div class="p-5">
+                <a href="#">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black"> {_hospital.name} </h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{_hospital.description}</p>
+                <Link href={`/department?hospital_id=${_hospital.id}`} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  Book an Appointment
+                  <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </Link>
+              </div>
             </div>
+          </div>
         ))}
+      </div>
     </>
   )
 }
