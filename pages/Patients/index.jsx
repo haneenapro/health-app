8ab906a3926f3710f0b787457362f56510102d99
saran.baral <saranbrl35@gmail.com>
2023-07-25 21:25:30 +0545

    rename

--- pages/Patients/index.jsx
+++ pages/Patients/index.jsx
@@ -112,7 +112,7 @@ function Page({ userData }) {
               href='/Appointment'
             >
               <Syringe className='text-center' />
-              Book Appointment
+              Book Consultation
             </Link>
             <Link
               className='w-full sm:w-[300px] font-bold justify-self-center flex flex-col gap-4 items-center px-10 py-20 border drop-shadow-xl rounded-md text-blue-800 bg-white hover:bg-indigo-600 hover:text-white'
